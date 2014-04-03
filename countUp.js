53427499d4bb35b46e73c4c7ef68e152c6720ef6
Nammyeong Kim <nmkim@narantech.com>
2014-04-03 10:25:59 +0900

    Added new end value option in resume
    * Added handle set new progress value when progress changed.
    * Added progress callback.

--- countUp.js
+++ countUp.js
@@ -22,7 +22,7 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
         separator : ',', // character to use as a separator
         decimal : '.' // character to use as a decimal
     }
-    
+
     // make sure requestAnimationFrame and cancelAnimationFrame are defined
     // polyfill for browsers without native support
     // by Opera engineer Erik Möller
@@ -50,11 +50,10 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
     }
 
     var self = this;
-    
+
     this.d = (typeof target === 'string') ? document.getElementById(target) : target;
     this.startVal = Number(startVal);
     this.endVal = Number(endVal);
-    this.countDown = (this.startVal > this.endVal) ? true : false;
     this.startTime = null;
     this.timestamp = null;
     this.remaining = null;
@@ -63,20 +62,24 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
     this.decimals = Math.max(0, decimals || 0);
     this.dec = Math.pow(10, this.decimals);
     this.duration = duration * 1000 || 2000;
-    
+    this.resumeDuration = 1000;
+    this.resumeCallback = null;
+    this.lastProgress = -1;
+    _refreshIsCountDown();
+
     // Robert Penner's easeOutExpo
     this.easeOutExpo = function(t, b, c, d) {
         return c * (-Math.pow(2, -10 * t / d) + 1) * 1024 / 1023 + b;
     }
     this.count = function(timestamp) {
-        
+
         if (self.startTime === null) self.startTime = timestamp;
 
         self.timestamp = timestamp;
 
         var progress = timestamp - self.startTime;
         self.remaining = self.duration - progress;
-        
+
         // to ease or not to ease
         if (self.options.useEasing) {
             if (self.countDown) {
@@ -93,29 +96,45 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
                 self.frameVal = self.startVal + (self.endVal - self.startVal) * (progress / self.duration);
             }
         }
-        
+
         // decimal
         self.frameVal = Math.round(self.frameVal*self.dec)/self.dec;
-        
+
         // don't go past endVal since progress can exceed duration in the last frame
         if (self.countDown) {
             self.frameVal = (self.frameVal < self.endVal) ? self.endVal : self.frameVal;
         } else {
             self.frameVal = (self.frameVal > self.endVal) ? self.endVal : self.frameVal;
         }
-        
+
         // format and print value
-        self.d.innerHTML = self.formatNumber(self.frameVal.toFixed(self.decimals));
-               
+        var progressVal = self.formatNumber(self.frameVal.toFixed(self.decimals));
+        if (self.lastProgress != progressVal) {
+            self.lastProgress = progressVal;
+
+            self.d.innerHTML = progressVal;
+            // fire progress callback
+            if (self.progressCallback != null) self.progressCallback(progressVal);
+        };
+
         // whether to continue
         if (progress < self.duration) {
             self.rAF = requestAnimationFrame(self.count);
-        } else {
-            if (self.callback != null) self.callback();
+        } else if (self.resumeCallback != null){
+            self.resumeCallback();
+        } else if (self.completeCallback != null) {
+            self.completeCallback();
         }
-    }  
-    this.start = function(callback) {
-        self.callback = callback;
+    }
+    this.start = function(progressCallback, completeCallback) {
+        // Addes callbacks
+        self.progressCallback = progressCallback;
+        self.completeCallback = completeCallback;
+
+        // brush-up init val.
+        self.resumeCallback = null;
+        self.lastProgress = -1;
+
         // make sure values are valid
         if (!isNaN(self.endVal) && !isNaN(self.startVal)) {
             self.rAF = requestAnimationFrame(self.count);
@@ -133,12 +152,38 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
         cancelAnimationFrame(self.rAF);
         self.d.innerHTML = self.formatNumber(self.startVal.toFixed(self.decimals));
     }
-    this.resume = function() {
+    this.resume = function(newEndVal, duration) {
         self.startTime = null;
-        self.duration = self.remaining;
-        self.startVal = self.frameVal;
-        requestAnimationFrame(self.count);
+        self.resumeCallback = null;
+        if (newEndVal != null && duration != null) {
+            _refreshIsCountDown();
+            // refresh values.
+            if (this.isRunnig()) {
+                self.duration = self.resumeDuration;
+                self.startVal = self.frameVal;
+
+                cancelAnimationFrame(self.rAF);
+                requestAnimationFrame(self.count);
+
+                this.resumeCallback = function() {
+                    _handleResumeCount(newEndVal, duration);
+                }
+            } else {
+                _handleResumeCount(newEndVal, duration);
+            }
+        } else {
+            // keep originals.
+            self.startTime = null;
+            self.duration = self.remaining;
+            self.startVal = self.frameVal;
+            requestAnimationFrame(self.count);
+        }
     }
+    this.isRunnig = function() {
+        // TODO: impl.
+        return self.lastProgress != self.endVal;
+    }
+    // TODO: impl end() with animation?
     this.formatNumber = function(nStr) {
         nStr += '';
         var x, x1, x2, rgx;
@@ -152,6 +197,18 @@ function countUp(target, startVal, endVal, decimals, duration, options) {
             }
         }
         return x1 + x2;
+    }
+
+    function _handleResumeCount(newEndVal, duration) {
+        self.startTime = null;
+        self.duration = duration * 1000 || self.duration;
+        self.startVal = self.frameVal;
+        self.endVal = newEndVal;
+        self.rAF = requestAnimationFrame(self.count);
+    }
+
+    function _refreshIsCountDown() {
+        this.countDown = (this.startVal > this.endVal) ? true : false;
     }
 
     // format startVal on initialization
